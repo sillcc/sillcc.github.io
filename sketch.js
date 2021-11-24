e96ab86dc79163369f230812703faecec19b66a9
silviuchiriac <6172099+silviuchiriac@users.noreply.github.com>
2021-11-24 01:08:39 +0100

    new

--- sketch.js
+++ sketch.js
@@ -1,164 +1,79 @@
-// recreation of demos in the book "Generative Design: visualize, program, and create with Processing"
-// P.1.2.3
-// Color palettes from rules
-// based on P_1_2_3_01.pde (https://github.com/generative-design/Code-Package-Processing-3.x/blob/master/01_P/P_1_2_3_01/P_1_2_3_01.pde)
-// interactions
-//	Mouse:  x/y: palette sizes
-//	Keys: s: save png; 1/2: switch color models 
-//
-// schien@mail.ncku.edu.tw, 20201014
-
-let tileCountX = 7;
-let tileCountY = 7;
-// arrays for color components values
-let hueValues = [];
-let saturationValues = [];
-let brightnessValues = [];
+let _nsRate;
+let _maxPoint;
+let _aryObject = [];
+let _objectNum;
+let _bgColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-	colorMode(HSB, 360, 100, 100, 100);
+  frameRate(60);
+  colorMode(HSB, 360, 100, 100, 255);
   noStroke();
-  // noLoop();
-	
-  // init with random values
-  // for (let i=0; i<tileCountX; i++) {
-	pal();
+  _objectNum = 6;
+  _nsRate = 0.0001;
+  _maxPoint = 100;
+
+  for (let i = 0; i < _objectNum; i++) {
+    _aryObject.push(new line());
+  }
+  _bgColor = color(0, 0, 83);
 }
 
 function draw() {
-	// background(0, 0, 100);
-	
-	// count every tile
-	let counter = 0;
-	let currentTileCountX = int(map(mouseX, width, 0, 1, tileCountX));
-  let currentTileCountY = int(map(mouseY, height, 0, 1, tileCountY));
+  clear();
+  background(_bgColor);
 
-  // let currentTileCountX = int(random(0, tileCountX));
-  // let currentTileCountY = int(random(0, tileCountY));
- 
+  for (let i = 0; i < _objectNum; i++) {
+    _aryObject[i].update();
+    _aryObject[i].draw();
+  }
+}
 
-	let tileWidth = width / float(currentTileCountX);
-	let tileHeight = height / float(currentTileCountY);
-	
-	for (let gridY=0; gridY<tileCountY; gridY++) {
-		for (let gridX=0; gridX<tileCountX; gridX++) {
-			let posX = tileWidth*gridX;
-			let posY = tileHeight*gridY;
-			let index = counter % currentTileCountX;
-			
-			fill(hueValues[index], saturationValues[index], brightnessValues[index]);
-			rect(posX, posY, tileWidth, tileHeight);
-			counter++;
-		}
-	}
+class line {
+  constructor() {
+    this.nsX = random(100);
+    this.nsY = random(100);
+    this.color = color(random(360), 100, 100, 255);
+    this.sw = random(width/20, width/4);
+    this.aryPoints = [];
+  }
+  update() {
+    this.nsX += _nsRate;
+    this.nsY += _nsRate;
+    this.aryPoints.unshift([
+      width/3 * cos(8*PI*noise(this.nsX)),
+      height/3 * sin(8*PI*noise(this.nsY))
+    ]);
+    while (this.aryPoints.length > _maxPoint) {
+      this.aryPoints.pop();
+    }
+  }
+  draw() {
+    fill(this.color);
+    push();
+    translate(width/2, height/2);
+    let ang = atan2(
+                    this.aryPoints[0][1] - this.aryPoints[this.aryPoints.length-1][1], 
+                    this.aryPoints[0][0] - this.aryPoints[this.aryPoints.length-1][0]
+                    );
+    beginShape();
+    vertex(this.aryPoints[0][0] + this.sw * cos(ang + PI/2),
+            this.aryPoints[0][1] + this.sw * sin(ang + PI/2));
+    vertex(this.aryPoints[0][0] + this.sw * cos(ang - PI/2),
+            this.aryPoints[0][1] + this.sw * sin(ang - PI/2));
+    vertex(this.aryPoints[this.aryPoints.length-1][0] + this.sw * cos(ang - PI/2),
+            this.aryPoints[this.aryPoints.length-1][1] + this.sw * sin(ang - PI/2));
+    vertex(this.aryPoints[this.aryPoints.length-1][0] + this.sw * cos(ang + PI/2),
+            this.aryPoints[this.aryPoints.length-1][1] + this.sw * sin(ang + PI/2));
+    endShape(CLOSE);
+    pop();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function render() {
   save("wallpaper.jpg");
-}
-
-function pal() {
-  for (let i = 0; i < 2; i++) {
-    hueValues[i] = int(random(0, 360));
-    saturationValues[i] = int(random(0, 100));
-    brightnessValues[i] = int(random(0, 100));
-  }
-}
-
-// function keyPressed() {
-// 	switch (key) {
-// 		case '1':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = int(random(0, 360));
-// 				saturationValues[i] = int(random(0, 100));
-// 				brightnessValues[i] = int(random(0, 100));
-// 			}
-// 			break;
-// 		case '2':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = int(random(0, 360));
-// 				saturationValues[i] = int(random(0, 100));
-// 				brightnessValues[i] = 100;
-// 			}
-// 			break;
-// 		case '3':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = int(random(0, 360));
-// 				saturationValues[i] = 100;
-// 				brightnessValues[i] = int(random(0, 100));
-// 			}
-// 			break;
-// 		case '4':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = 0;
-// 				saturationValues[i] = 0;
-// 				brightnessValues[i] = int(random(0, 100));
-// 			}
-// 			break;
-// 		case '5':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = 195;
-// 				saturationValues[i] = 100;
-// 				brightnessValues[i] = int(random(0, 100));
-// 			}
-// 			break;
-// 		case '6':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = 195;
-// 				saturationValues[i] = int(random(0, 100));
-// 				brightnessValues[i] = 100;
-// 			}
-// 			break;
-// 		case '7':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = int(random(0, 180));
-// 				saturationValues[i] = int(random(80, 100));
-// 				brightnessValues[i] = int(random(50, 90));
-// 			}
-// 			break;
-// 		case '8':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				hueValues[i] = int(random(180, 360));
-// 				saturationValues[i] = int(random(80, 100));
-// 				brightnessValues[i] = int(random(50, 90));
-// 			}
-// 			break;
-// 		case '9':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				if (i%2 === 0) {
-// 					hueValues[i] = int(random(0, 360));
-// 					saturationValues[i] = 100;
-// 					brightnessValues[i] = int(random(0, 100));
-// 				} else {
-// 					hueValues[i] = 195;
-// 					saturationValues[i] = int(random(0, 100));
-// 					brightnessValues[i] = 100;
-// 				}
-// 			}
-// 			break;
-// 		case '0':
-// 			for (let i=0; i<tileCountX; i++) {
-// 				if (i%2 === 0) {
-// 					hueValues[i] = 192;
-// 					saturationValues[i] = int(random(0, 100));
-// 					brightnessValues[i] = int(random(10, 100));
-// 				} else {
-// 					hueValues[i] = 273;
-// 					saturationValues[i] = int(random(0, 100));
-// 					brightnessValues[i] = int(random(10, 90));
-// 				}
-// 			}
-// 			break;
-// 		case 's':
-// 		case 'S':
-// 			save("color-palettes.png");
-// 			break;
-// 		default:
-// 	}
-// }
-
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
